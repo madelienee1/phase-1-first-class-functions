8064ac10e7c03b3521329dc7c58ca8595ce79ee9
Madeliene <madelienestiller@gmail.com>
2024-02-27 12:50:00 +1000

    lab complete

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("Hello");
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        console.log("Hello");
+    }
+}
